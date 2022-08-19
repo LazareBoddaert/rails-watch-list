2fe28346207ab6c113a4955b9304942227409912
Lazare Boddaert <lazare.boddaert@gmail.com>
2022-08-19 12:29:38 +0200

    new things

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,7 +18,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to lists_path, notice: 'Bookmark was successfully destroyed.'
+    redirect_to list_path(@bookmark.list), status: :see_other, notice: 'Bookmark was successfully destroyed.'
   end
 
   private
